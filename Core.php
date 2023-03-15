d2417d79725a32153cc4c0f696a250c0fafc3009
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-15 11:16:21 +0300

    Fix: more

--- Core.php
+++ Core.php
@@ -382,6 +382,11 @@ class Core extends SchemaAbstract
 	 */
 	public function processingClassifierGroups(ClassifierDataContract $classifier, Reader $reader)
 	{
+		if($reader->schema_version === '3.1' && $reader->getFiletype() !== 'groups')
+		{
+			return;
+		}
+
 		if(!$classifier->hasGroups())
 		{
 			$this->log()->info(__('Classifier groups is empty.', 'wc1c-main'));
@@ -695,6 +700,11 @@ class Core extends SchemaAbstract
 	 */
 	public function processingClassifierProperties(ClassifierDataContract $classifier, Reader $reader)
 	{
+		if($reader->schema_version === '3.1' && $reader->getFiletype() !== 'propertiesGoods')
+		{
+			return;
+		}
+
 		if(!$classifier->hasProperties())
 		{
 			$this->log()->info(__('Classifier properties is empty.', 'wc1c-main'), ['filetype' => $reader->getFiletype()]);
