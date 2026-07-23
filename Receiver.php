98d0167ac8efbe0a9eb6c98a91f53ec3c9e8d410
Frescoref <frescoref@gmail.com>
2026-07-23 20:10:54 +0300

    Fix: more

--- Receiver.php
+++ Receiver.php
@@ -264,7 +264,7 @@ final class Receiver extends ReceiverAbstract
         }
 
         $credentials['login'] = sanitize_text_field($_SERVER['PHP_AUTH_USER']);
-        $credentials['password'] = isset($_SERVER['PHP_AUTH_PW']) ? (string) $_SERVER['PHP_AUTH_PW'] : '';
+        $credentials['password'] = isset($_SERVER['PHP_AUTH_PW']) ? sanitize_text_field(wp_unslash($_SERVER['PHP_AUTH_PW'])) : '';
 
         $this->core()->log()->debug(esc_html__('Credentials extracted from PHP_AUTH headers.', 'wc1c-main'), ['login' => $credentials['login'], 'password_length' => strlen($credentials['password'])]);
 
@@ -308,10 +308,11 @@ final class Receiver extends ReceiverAbstract
 
 		$session_name = session_name();
 
-		if(session_status() === PHP_SESSION_NONE)
+		if(session_status() === PHP_SESSION_NONE && defined('WC1C_RECEIVER_REQUEST') && WC1C_RECEIVER_REQUEST)
 		{
-			$this->core()->log()->debug(esc_html__('PHP session none, start new PHP session.', 'wc1c-main'));
-			session_start();
+            $this->core()->log()->debug(esc_html__('PHP session none, start new PHP session.', 'wc1c-main'));
+
+            session_start();
 		}
 
 		$session_id = session_id();
@@ -414,7 +415,14 @@ final class Receiver extends ReceiverAbstract
 		{
 			$warning = esc_html__('Session verification failed: Session ID mismatch.', 'wc1c-main');
 
-			$this->core()->log()->warning($warning, ['client_session_id' => $_COOKIE[$session_name], 'server_session_id' => $session_id]);
+            $this->core()->log()->warning
+            (
+                $warning,
+                [
+                    'client_session_id' => isset($_COOKIE[$session_name]) ? sanitize_text_field(wp_unslash($_COOKIE[$session_name])) : '',
+                    'server_session_id' => $session_id,
+                ]
+            );
 
 			if($send_response)
 			{
@@ -424,12 +432,17 @@ final class Receiver extends ReceiverAbstract
 			return false;
 		}
 
+
 		if(session_status() === PHP_SESSION_NONE)
 		{
 			session_id($session_id);
 
-			$this->core()->log()->debug(esc_html__('PHP session none, restart last PHP session.', 'wc1c-main'), ['session_id' => $session_id]);
-			session_start();
+            if(defined('WC1C_RECEIVER_REQUEST') && WC1C_RECEIVER_REQUEST)
+            {
+                $this->core()->log()->debug(esc_html__('PHP session none, restart last PHP session.', 'wc1c-main'), ['session_id' => $session_id]);
+
+                session_start();
+            }
 		}
 
 		return true;
