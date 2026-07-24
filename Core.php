12fb4104d43dc4ee0c421c48cde26ba0cf119e45
Frescoref <frescoref@gmail.com>
2026-07-24 19:14:11 +0300

    Fix: more

--- Core.php
+++ Core.php
@@ -336,7 +336,8 @@ class Core extends SchemaAbstract
 
 			ob_start();
 			nocache_headers();
-			do_action($wc1c_receiver_action);
+            // todo: optimize via remove dynamic  hook
+            do_action($wc1c_receiver_action);  // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
 			ob_end_clean();
 		}
 
